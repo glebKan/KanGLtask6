1e2ac6c078f43cff1e0ced38a67810fde401a24b
Kanareykin Gleb <gleb.canareykin2018@yandex.ru>
2022-04-02 14:07:55 +0300

    Добавьте файлы проекта.

---
+++ peregruzka/peregruzka.h
@@ -0,0 +1,14 @@
+#ifndef PEREGRUZKA_H
+#define PEREGRUZKA_H
+#include <iostream>
+
+using namespace std;
+void puzirik(double*, int);
+void puzirik(int*, int);
+double* create(double*, int);
+void full(double*, int);
+void print(double*, int);
+int* create(int* mas, int size);
+void full(int* mas, int size);
+void print(int* mas, int size);
+#endif
